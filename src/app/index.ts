b0a420e82743e088f5f68b0042218c22b49cb338
Roie Golst <102251357+roieGolst@users.noreply.github.com>
2023-02-13 16:51:47 +0000

    WIP rsa implemtion

--- src/app/index.ts
+++ src/app/index.ts
@@ -19,22 +19,27 @@ const plainText: string = "Roie is the men";
 
 const encrypter = Encryption.factory();
 
-const publcKey = encrypter.getKeysPair({key: `-----BEGIN PRIVATE KEY-----
-MIIBVgIBADANBgkqhkiG9w0BAQEFAASCAUAwggE8AgEAAkEA3h0/0NYzwQWPEMtd
-VSnvI6W4Jm9O27MSlLzdZc9FHTA41svrApmf4GvhKqw1BI/9Ka8j+kBLw4rWqday
-UMPsWwIDAQABAkAEpz44BNTOvWwS4P/Ske9Votopy/kt9yFzhkVpJZ+ADgGSSkhA
-DRLrWbhPr+0MvcuuhPI/Lk3UlDJ6b07/jRxhAiEA+b2KtTE3ny6JvZC6dPihe8+X
-xbydF7jDC4vVYVt/wTECIQDjrnHGEgWHDGFc8/T4C2awR4PtabljnAWvZ4HjWY7D
-SwIhAJNEeCT8zuXpVYQZcjpD20XdVoxd+uzgIPQQE1YD1MQhAiEA0JBCBJEJozZC
-EiYfpwC6pow8W8TXYFnSE2Wb5Clrz5UCIQCXCTiIakAP7XMDJFA9S3vz8g+Gyqhe
-ifFxHSTo/nhSdw==
------END PRIVATE KEY-----`, format: "pkcs8"});
+// const publcKey = encrypter.getKeysPair({key: `-----BEGIN PUBLIC KEY-----
+// MFwwDQYJKoZIhvcNAQEBBQADSwAwSAJBAINtfIZH/Xrd+Ld8Tkv82xMqYlO6uXzd
+// 9EEnolbyQehttBYn8WVSZgruDxpGspp8/aaxpfoiGv8HbThIC/XV9A8CAwEAAQ==
+// -----END PUBLIC KEY-----`, format: "pkcs8-public"});
 
-const encryptedData = publcKey.encrypt({content: plainText});
+// const encryptedData = publcKey.encrypt({content: plainText});
 
-console.log(encryptedData);
+const encryptedData = `JTh2w2HE1FwAO82AherCjOC0rMqeiqhJVkJiearEvAOLy0R/KLWrOo/4g1gYpJIuq58uh0SZMdTphXVHHuxTOw==`;
 
+// console.log(encryptedData);
 
-// const decryptData = rsa.decrypt({content: encryptedData});
+const privateKey = encrypter.getKeysPair({key: `-----BEGIN RSA PRIVATE KEY-----
+MIIBOQIBAAJBAINtfIZH/Xrd+Ld8Tkv82xMqYlO6uXzd9EEnolbyQehttBYn8WVS
+ZgruDxpGspp8/aaxpfoiGv8HbThIC/XV9A8CAwEAAQJAOZzwPokmZAsUMZMLW6Iv
+gvpMejnbgBJoNXujXYv7OmDfFPmEXDL8iVE5UCqJ9eQeZq6mAA+aDhNMm+M72Bgd
+CQIhAOrZtBg6UyVyrisYaNIVipKMurpvDDF3S2p6qE3NXunjAiEAj0NzfSgv49/i
+1n61iVToZmOsuOM+huGI7XW91AgDFOUCIB5+0nHq8O0F7TPPYZ0WCv5oo76duNmb
+ggF7l+syZca9AiAjdp/9CFiqgP5m+GC5ySHqTpwv/Fi7DNNXbMD73qvcnQIgYV4C
+yIyBTD7H/F6CioOkKuEAhUVjGOo7BEMbdajIA7E=
+-----END RSA PRIVATE KEY-----`, format: "private"});
 
-// console.log(`Decrypted data: ${decryptData}`);``
+const decryptedData = privateKey.decrypt({content: encryptedData});
+
+console.log(decryptedData);
